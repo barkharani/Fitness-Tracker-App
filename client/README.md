bc89268cb1c5071d9b231a7c3d434f6e921fc59f
Barkha Rani <ranib1@hawkmail.newpaltz.edu>
2019-04-23 12:01:31 -0400

    updated homepage

---
+++ client/README.md
@@ -0,0 +1,27 @@
+# Fitness Tracker App UI
+
+> Presentation Layer for Fitness Tracker App
+
+### How do I get set up? ###
+
+* Clone the repo to your local machine.
+
+### Build Setup ###
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+
+# build for production and view the bundle analyzer report
+npm run build --report
+```
+
+
+
+
---
+++ client/src/router/index.js
@@ -0,0 +1,85 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import store from './../vuex/store';
+import Signin from '../components/Signin';
+import Signup from '../components/Signup';
+import dashboardComponent from './../components/common/Dashboard';
+import pageNotFoundComponent from './../components/common/PageNotFound';
+import Home from './../components/home/Home';
+import Excersice from './../components/excersice/Excersice';
+import FatToFit from './../components/workouts/FatToFit';
+import FriendList from './../components/friends/FriendList';
+import Profile from './../components/profile/Profile';
+import auth from '../auth';
+
+
+Vue.use(Router);
+
+export default new Router({
+  // mode: 'history',
+  hashbang: false,
+  routes: [{
+    path: '/',
+    name: 'Root',
+    redirect: 'signin',
+  },
+  {
+    path: '/signin',
+    name: 'Signin',
+    beforeEnter: function(to, from, next) {
+        if (auth.checkAuth() && auth.getUser()) {
+          next({ name: 'HomeView' });
+        } else {
+          next();
+        }
+        
+    },
+    component: Signin,
+    
+  },
+  {
+    path: '/signup',
+    name: 'Signup',
+    component: Signup,
+    
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    redirect: {
+      name: 'HomeView',
+    },
+    component: dashboardComponent,
+    children: [{
+      path: '/dashboard/home',
+      name: 'HomeView',      
+      component: Home,
+    },
+    {
+      path: '/dashboard/friends',
+      name: 'FriendListView',      
+      component: FriendList,
+    },
+    {
+      path: '/dashboard/excersice',
+      name: 'ExcersiceView',      
+      component: Excersice,
+    },
+    {
+      path: '/dashboard/fattofit',
+      name: 'FatToFitView',      
+      component: FatToFit,
+    },
+    {
+      path: '/dashboard/profile',
+      name: 'ProfileView',      
+      component: Profile,
+    },
+    ],
+  },
+  {
+    path: '*',
+    component: pageNotFoundComponent,
+  },
+  ],
+});
